42e031b483130178560d643824a073e5b1388f74
Sanket Harvande <sanketharvande49@gmail.com>
2022-08-02 07:38:43 +0530

    modified changes

---
+++ objects.js
@@ -0,0 +1,42 @@
+// object -> is a group of key : value pair
+// key : value -> property
+// key : function -> method
+
+let cap = {
+
+    name : "Steve",
+    lastname : "Rogers",
+    address : {
+        city : "Manhatten",
+        state : "New York",
+    },
+    
+    age : 35, 
+    isAvenger : true,
+    movies : ["first avenger", "winter soldier", "civil war"], 
+    sayHi : function() {
+        console.log("Cap says hi");
+    }
+};
+
+// get
+// console.log(cap.name);
+// console.log(cap.lastname);
+// console.log(cap.address.city);
+// console.log(cap.address.state);
+// console.log(cap.age);
+// console.log(cap.isAvenger);
+// console.log(cap.movies[0]);
+// cap.sayHi();
+
+// set / update 
+
+
+
+
+
+
+
+
+
+
