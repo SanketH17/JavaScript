87abd760f7683db1eb215248476b447243279299
Sanket Harvande <sanketharvande49@gmail.com>
2022-08-02 06:36:21 +0530

    first commit

---
+++ intro.js
@@ -0,0 +1,24 @@
+console.log("Hello javascript");
+
+//declare variables
+let a; 
+console.log(a);
+
+a = 10;
+
+a = 10.1;
+
+a = "hello i am a string";
+console.log(a);
+a = "hello i am also a string";
+
+console.log("Variable a contains ", a);
+
+a = true;
+console.log(a);
+
+
+let number = 10;
+for(let i = 0; i <= number; i++){
+    console.log("Number is ", i);
+}
